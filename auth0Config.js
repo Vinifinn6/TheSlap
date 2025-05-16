04c3426d1733fe20e6703637780adb4db5451a85
Vinifinn6 <32985112+Vinifinn6@users.noreply.github.com>
2025-05-15 23:42:00 -0300

    Update auth0Config.js
    1

--- auth0Config.js
+++ auth0Config.js
@@ -1,7 +1,7 @@
 // Configuração do Auth0
 const auth0Config = {
-  domain: "seu-dominio.auth0.com",
-  clientId: "seu-client-id-auth0",
+  domain: "vinifinn6.us.auth0.com",
+  clientId: "OuKs70nxKcQCb43urRJ8LbiHpOcnm0Ui",
   audience: "https://api.theslap.com", // API identifier (opcional)
   redirectUri: window.location.origin,
   scope: "openid profile email",
