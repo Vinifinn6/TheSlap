dd800ead8b311f6f7160c5778442d8914aacfde9
Vinifinn6 <32985112+Vinifinn6@users.noreply.github.com>
2025-05-16 23:37:10 -0300

    Add files via upload

---
+++ auth-config.js
@@ -0,0 +1,7 @@
+export const auth0Config = {
+  domain: "vinifinn6.us.auth0.com",
+  clientId: "OuKs70nxKcQCb43urRJ8LbiHpOcnm0Ui",
+  audience: "https://api.theslap.com",
+  redirectUri: window.location.origin,
+  scope: "openid profile email",
+};
