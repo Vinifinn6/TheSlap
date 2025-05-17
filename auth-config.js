b77034c072251b101c64c4f6395b15237e4a97a9
Vinifinn6 <32985112+Vinifinn6@users.noreply.github.com>
2025-05-16 23:41:37 -0300

    Update auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -1,7 +1,1 @@
-export const auth0Config = {
-  domain: "vinifinn6.us.auth0.com",
-  clientId: "OuKs70nxKcQCb43urRJ8LbiHpOcnm0Ui",
-  audience: "https://api.theslap.com",
-  redirectUri: window.location.origin,
-  scope: "openid profile email",
-};
+
